b3818e1c48f0dcd68243fc4cb1705fb285727096
Jose-silvaa <jvitorfacanha@gmail.com>
2025-11-07 22:10:17 -0300

    first

---
+++ library.API/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace library.API.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+
+public class UserController : ControllerBase
+{
+    
+}
---
+++ library.Domain/Domain/Infrastructure/CQRS/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace library.Infrastructure.CQRS;
+
+public interface IBaseModel
+{
+    Guid Id { get; set; }
+}
+
+public class BaseModel : IBaseModel
+{
+    public virtual Guid Id { get; set; }
+}
